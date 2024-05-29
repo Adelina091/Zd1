dbf012d82b89d1e5621751414c3290a128f8c490
Adelina091 <adelina.kabirova19@gmail.com>
2024-05-29 21:10:32 +0500

    lk

---
+++ src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Class Component</h1>
+        <h1>
+          <p>Class Component 2</p>
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
